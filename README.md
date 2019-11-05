a4e6b27bb43dafd109ae205b604f4493df930077
cjfrontanes0401 <r3doppz@gmail.com>
2019-11-05 10:37:25 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # weather-now
+
+get user input of the city they want weather from
+
+get weather for the city the user input
+    if error let the user know there was an error and try again
+    if there is no weather available error try again 
+    else store the weather for the user city
+show the weather to the user or show the error     
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.set("view engine", "ejs");
 
 //middleware
 app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*
     ROUTES
@@ -16,6 +18,10 @@ app.use(express.static("./public"));
 app.get("/", function (req, res){
     res.render("home.ejs");
 }); 
+
+app.post("/whatever-we-want", function(req, res){
+    console.log(req);
+});
 
 app.listen(3000, function(){
     console.log("server is live on port: 3000");
